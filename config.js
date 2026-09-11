14b090f7d5e8448a5bc3f277b5c10b51ec0f01d4
editorgaadu-cloud <editorgaadu@gmail.com>
2026-09-12 00:59:13 +0530

    Configure Creator Console backend function

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.CHILLOUT_CONFIG={SUPABASE_URL:'https://gobskccwbnkclgmwlgbm.supabase.co',SUPABASE_ANON_KEY:'sb_publishable_b6117-eXqIvJ48d9-epGzA_0n_2Wn0h',STAFF_FN:'manage-staff-v2'};
+window.CHILLOUT_CONFIG={SUPABASE_URL:'https://gobskccwbnkclgmwlgbm.supabase.co',SUPABASE_ANON_KEY:'sb_publishable_b6117-eXqIvJ48d9-epGzA_0n_2Wn0h',STAFF_FN:'manage-staff-v2',CREATOR_FN:'creator-platform'};
